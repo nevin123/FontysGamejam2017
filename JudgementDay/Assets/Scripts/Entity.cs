c877dbfaaf40477f25deac938560f1e6d2057b20
Koen <kndschilders@gmail.com>
2017-11-09 17:35:07 +0100

    Added entitymanager

---
+++ JudgementDay/Assets/Scripts/Entity.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour {
+
+	public EntityType Type;
+
+	public ItemType Item;
+
+	void Start() {}
+
+	void Update() {}
+
+	public string ToString() {
+		return this.Type + ", " + this.Item;
+	}
+}
---
+++ JudgementDay/Assets/Scripts/EntityManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityManager : MonoBehaviour {
+
+	public List<Entity> AvailableEntities = new List<Entity>();
+
+	public Transform EntityContainer;
+
+	private Queue<Entity> entities = new Queue<Entity> (new Entity[] { });
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < 10; i++) {
+			Entity entity = Instantiate (AvailableEntities [0]);
+			entity.Item = ItemType.Knife;
+			entity.Type = EntityType.Biker;
+			entity.transform.SetParent (this.EntityContainer.transform);
+
+			this.entities.Enqueue(entity);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+}
