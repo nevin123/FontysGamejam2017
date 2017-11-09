06913f3e411438ef0bf4e125353f15c17e7dd185
wouttotti <wout_kamp@hotmail.com>
2017-11-09 19:31:25 +0100

    InputManager and Spawner

---
+++ JudgementDay/Assets/Scripts/Spawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+
+    public GameObject[] Spots;
+    public GameObject Entity;
+
+	// Use this for initialization
+	void Start () {
+		foreach(GameObject s in Spots)
+        {
+            Instantiate(Entity, s.transform);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
